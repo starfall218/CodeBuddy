0ee56fee641c9e453c8ab7c55154e61822d80c27
Starfall218 <starfall101440@gmail.com>
2024-11-09 07:52:37 +0530

    Prototype for the code editor
    Not yet well designed, front page still needs work.
    
    Co-Authored-By: SunavaG <169614528+SunavaG@users.noreply.github.com>

---
+++ Editor/Editor JavaScript/HTMLeditor.js
@@ -0,0 +1,9 @@
+const run = () => {
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;  
+    let output = document.getElementById("run");
+
+    output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode +"</style>";
+    output.contentWindow.eval(jsCode)
+  }
